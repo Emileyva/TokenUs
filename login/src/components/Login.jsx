1b8cc250f4adb827b9fb3c34fa21ce58751f7e3f
Emileyva <emiliano.leyvagallarati@gmail.com>
2024-08-17 14:55:38 -0300

    .

--- login/src/components/Login.jsx
+++ login/src/components/Login.jsx
@@ -8,6 +8,7 @@ import { Label } from './ui/label';
 import { Button } from './ui/button';
 import { Checkbox } from './ui/checkbox';
 import z from 'zod';
+import { CiLock } from "react-icons/ci";
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -56,48 +57,57 @@ const Login = () => {
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-b from-blue-900 to-teal-600">
-      <form onSubmit={handleSubmit} className="bg-gray-900 bg-opacity-75 p-8 rounded-lg shadow-lg w-full max-w-sm">
-        <h2 className="text-teal-400 text-center text-xl mb-6">CUSTOMER LOGIN</h2>
-        <div className="mb-4">
-          <Label htmlFor="email" className="text-gray-300 mb-1">
+      <form onSubmit={handleSubmit} className="bg-gray-900 bg-opacity-75 pb-8 rounded-3xl shadow-lg w-full max-w-sm ">
+        <div className='flex justify-center items-center bg-teal-500 rounded-t-3xl h-20'>
+          <h2 className="text-teal-dark text-center text-xl ">CUSTOMER LOGIN</h2>
+        </div>
+        <div className="mb-4 px-8 pt-10">
+          <Label htmlFor="email" className="text-gray-300 mb-1" >
             Email ID
           </Label>
           <Input
+            placeholder="Email"
             id="email"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className={`w-full px-3 py-2 border ${errors.email ? 'border-red-600' : 'border-gray-600'} rounded bg-gray-800 text-gray-300 focus:outline-none focus:border-teal-400`}
+            className={`w-full px-3 py-2 border ${errors.email ? 'border-red-600' : 'border-gray-600'} rounded bg-transparent text-white focus:outline-none focus:border-teal-400`}
           />
           {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
         </div>
-        <div className="mb-4">
+        <div className="mb-4 px-8">
+
           <Label htmlFor="password" className="text-gray-300 mb-1">
             Password
           </Label>
           <Input
+            placeholder="Password"
             id="password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className={`w-full px-3 py-2 border ${errors.password ? 'border-red-600' : 'border-gray-600'} rounded bg-gray-800 text-gray-300 focus:outline-none focus:border-teal-400`}
+            className={`w-full px-3 py-2 border-b-4 ${errors.password ? 'border-red-600' : 'border-b-white'} rounded bg-transparent text-white focus:outline-none focus:border-teal-400`}
           />
           {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
+
         </div>
-        <div className="flex items-center justify-between mb-4">
+
+        <div className="flex items-center justify-between mb-4 px-8">
           <div className="flex items-center">
             <Checkbox id="remember" />
             <Label htmlFor="remember" className="ml-2 text-gray-400">
               Remember me
             </Label>
           </div>
-          <a href="#" className="text-teal-400 text-sm">
+          <a href="#" className="text-teal-400 text-sm ">
             Forgot Password?
           </a>
         </div>
-        <Button type="submit" className="w-full bg-teal-500 text-gray-900 py-2 rounded focus:outline-none hover:bg-teal-600">
-          LOGIN
-        </Button>
+        <div className='px-20'>
+          <Button type="submit" className="w-full bg-teal-500 text-white py-2 rounded focus:outline-none hover:bg-teal-600">
+            LOGIN
+          </Button>
+        </div>
       </form>
       <ToastContainer />
     </div>
